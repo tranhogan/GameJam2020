a1e95845ae3bd45a351e054b6c17a4c0e71656e0
Hogan Tran <tranhogan@yahoo.com>
2020-09-19 13:49:24 -0500

    Added basic start menu so far

---
+++ 2D Game/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+/*using System.Runtime.Hosting;*/
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.Load(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
