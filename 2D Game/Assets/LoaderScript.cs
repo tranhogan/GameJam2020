13783ecc95e43703bf65c166b2562df738b49c3d
Hogan Tran <tranhogan@yahoo.com>
2020-09-19 17:20:17 -0500

    Added Loader

---
+++ 2D Game/Assets/LoaderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderScript : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNext();
+        }
+    }
+
+    public void LoadNext()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    IEnumerable LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
