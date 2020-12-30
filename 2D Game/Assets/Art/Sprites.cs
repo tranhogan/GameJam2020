fa34b28a35930c7c149182287d43734294a53f83
hogant <tranhogan@yahoo.com>
2020-12-30 15:05:36 -0600

    Fixed GUI and Loader

---
+++ 2D Game/Assets/Art/Sprites.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sprites : MonoBehaviour
+{
+    public void clothes()
+    {
+
+    }
+
+    public void computer()
+    {
+
+    }
+
+    public void dog()
+    {
+
+    }
+
+    public void kermit()
+    {
+
+    }
+
+    public void notes()
+    {
+
+    }
+}
---
+++ 2D Game/Assets/UI/HappinessBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HappinessBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetMaxHappiness(int happiness)
+    {
+        slider.maxValue = happiness;
+        slider.value = happiness / 2;
+    }
+
+    public void SetHappiness(int happiness) 
+    {
+        slider.value = happiness / 2;
+    }
+}
