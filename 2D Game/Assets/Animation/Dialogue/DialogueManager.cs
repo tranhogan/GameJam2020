81901c0a54c307aaead6a81ccc0878abd7eaa386
hogant <tranhogan@yahoo.com>
2020-12-30 15:05:36 -0600

    Current project

---
+++ 2D Game/Assets/Animation/Dialogue/DialogueManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text dialogueText;
+
+    private Queue<string> sentences;
+    void Start()
+    {
+        sentences = new Queue<string>();   
+    }
+
+    public void StartDialogue(Dialogue dialogue) 
+    {
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine("TypeSentence", sentence);
+    }
+
+    IEnumerable TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+}
---
+++ 2D Game/Assets/UI/ButtonBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Prev()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Skip()
+    {
+
+    }
+}
